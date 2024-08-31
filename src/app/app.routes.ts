b07103965776a8252c7ad4d8699993df10b5b289
Gaussdrigo <alvarengar49@gmail.com>
2024-08-31 16:12:24 -0400

    modulo proveedor version estilos de abel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
-import { Routes } from '@angular/router';
-import { ProductoModule } from './producto/producto.module';
-
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: "producto", loadChildren: () => ProductoModule } // Carga el módulo de productos
+  
+  // Carga perezosa del módulo de productos
+  { path: "producto", loadChildren: () => import('./producto/producto.module').then(m => m.ProductoModule) },
+
+  // Carga perezosa del módulo de proveedores
+  { path: "proveedores", loadChildren: () => import('./proveedor/proveedor.module').then(m => m.ProveedorModule) }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

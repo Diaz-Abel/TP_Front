4a6795aaf1ad611695a62a020c88a445152ce850
Ariel Vallejos <arielito.vallejo@fpuna.edu.py>
2024-09-02 11:37:16 -0400

    Cambios en las rutas de redireccion del navbar para poder redirigir a proveedor  y en los estilos de proveedor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
+import { ProductoModule } from './producto/producto.module';
+import { JaulaModule } from './jaula/jaula.module';
+import { ProveedorModule } from './proveedor/proveedor.module';
+
 
 export const routes: Routes = [
-    { path: "producto", loadChildren: () => ProductoModule } // Carga el módulo de productos
+    { path: "producto", loadChildren: () => ProductoModule }, // Carga el módulo de productos
+    { path: "jaula", loadChildren: () => JaulaModule }, // Carga el módulo de jaulas
+    { path: "proveedor", loadChildren: () => ProveedorModule }
 ];
